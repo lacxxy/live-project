e00391c98c9d1a7cb2c79a9d4fa839eb06193c05
git <1196455953@qq.com>
2019-11-02 11:17:36 +0800

    餐厅模块

--- pages/rest/rest.js
+++ pages/rest/rest.js
@@ -5,7 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    "rest1":"漳州沙茶面",
+    "rest2": "闽南猪蹄饭",
+    "rest3": "东北饺子馆",
+    "rest4": "纸包鱼",
+    "rest5": "玫瑰十二道",
+    "evaluate1":"9.8",
+    "evaluate2": "9.3",
+    "evaluate3": "9.1",
+    "evaluate4": "9.0",
+    "evaluate5": "8.8",
+    "price1":"12",
+    "price2": "14",
+    "price3": "20",
+    "price4": "23",
+    "price5": "13",
   },
 
   /**
@@ -47,7 +61,15 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    wx.showNavigationBarLoading() //在标题栏中显示加载
 
+    setTimeout(function () {
+
+      wx.hideNavigationBarLoading() //完成停止加载
+
+      wx.stopPullDownRefresh() //停止下拉刷新
+
+    }, 1500);
   },
 
   /**
